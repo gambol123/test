39213befaf1d32be527f572bb83b01438847c691
gamkittisak <kittisak.p57@rsu.ac.th>
2019-01-20 15:04:13 +0700

    test

---
+++ index2.php
@@ -0,0 +1,13 @@
+<?php 
+$test=1;
+
+$array= array('1'=>'John', '2' => 'Jack');
+$test   = array('1',"test1",
+                '2',"test2",
+                '3',"test3" );
+  if(test)
+  {
+    echo "hello world";
+    
+  }
+?>
